9d678c4d43475a607d981e0c1cede27cd22cd16d
Yuri Leal da Cruz <yakirodmorfera@gmail.com>
2026-09-09 14:05:09 -0300

    Add Migrations

--- src/Ponto.Api/Program.cs
+++ src/Ponto.Api/Program.cs
@@ -18,6 +18,13 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PontoDbContext>();
+
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
